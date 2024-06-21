2cd719c14cb600cdfff312abf99d6a068318811c
David Humphreys <newky2k@users.noreply.github.com>
2024-06-21 11:48:02 +0100

    Cleaned compilation for unit tests

--- YamlSerializer/AssemblyInfo.cs
+++ YamlSerializer/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("134feee3-4ccb-42bf-89aa-95eef3cf9405")]
 
 #if DEBUG
-[assembly: InternalsVisibleTo("YamlSerializerTest")]
+[assembly: InternalsVisibleTo("YamlSerializerTestNew")]
 #endif
